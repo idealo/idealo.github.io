0bfb5263ba3d3c1beba588c660cde2449eaf7194
Dat Tran <datitran@gmail.com>
2019-01-31 15:46:52 +0100

    Add template from axelspringer.github.iofor idealo's landing page.

--- index.md
+++
@@ -1,5 +0,0 @@
----
-title: Product & Technology
-description: List of our open-source projects
-organization: idealo internet GmbH
----
---
+++ src/data/jobImages.js
@@ -0,0 +1,34 @@
+export default [
+    {
+        "imgUrl": "static/jobs_04.png",
+        "thumbnail": "static/thumbnails/jobs_04.png"
+    },
+    {
+        "imgUrl": "static/jobs_08.png",
+        "thumbnail": "static/thumbnails/jobs_08.png"
+    },
+    {
+        "imgUrl": "static/jobs_01.png",
+        "thumbnail": "static/thumbnails/jobs_01.png"
+    },
+    {
+        "imgUrl": "static/jobs_03.png",
+        "thumbnail": "static/thumbnails/jobs_03.png"
+    },
+    {
+        "imgUrl": "static/jobs_02.png",
+        "thumbnail": "static/thumbnails/jobs_02.png"
+    },
+    {
+        "imgUrl": "static/jobs_05.png",
+        "thumbnail": "static/thumbnails/jobs_05.png"
+    },
+    {
+        "imgUrl": "static/jobs_07.png",
+        "thumbnail": "static/thumbnails/jobs_07.png"
+    },
+    {
+        "imgUrl": "static/jobs_06.png",
+        "thumbnail": "static/thumbnails/jobs_06.png"
+    }
+]
