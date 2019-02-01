c5a0d3dcb91ca462f13a8e13166eb6e6b47a68eb
Dat Tran <datitran@gmail.com>
2019-02-01 17:09:01 +0100

    Remove xing from social as this breaks with mobile.

--- src/components/Socials/Socials.jsx
+++ src/components/Socials/Socials.jsx
@@ -9,7 +9,6 @@ export default (props) => {
             <a href='https://twitter.com/idealoTech' className='socials__item'><FontAwesome name='twitter' /></a>
             <a href='https://medium.com/idealo-tech-blog' className='socials__item'><FontAwesome name='medium' /></a>
             <a href='https://www.linkedin.com/company/idealo-internet-gmbh/' className='socials__item'><FontAwesome name='linkedin' /></a>
-            <a href='https://www.xing.com/companies/idealointernetgmbh' className='socials__item'><FontAwesome name='xing' /></a>
             <a href='https://www.instagram.com/idealo/' className='socials__item'><FontAwesome name='instagram' /></a>
             <a href='https://www.facebook.com/idealoDE/' className='socials__item'><FontAwesome name='facebook' /></a>
         </p>
