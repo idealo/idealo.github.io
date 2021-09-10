fe35ffe031f8a79e15d679831076e4212dda2c89
Bogdan Girman <bogdan.girman@idealo.de>
2021-09-10 10:00:35 +0200

    Remove project SageMaker Experiments Explorer
    It is not yet ready to be published publicly

--- src/data/projects.js
+++ src/data/projects.js
@@ -128,15 +128,5 @@ export default [
         "ownerLogo": "idealo",
         "projectId": "deckard",
         "language": "java"
-    },
-    {
-        "title": "SageMaker Experiments Explorer",
-        "description": "Easily explore SageMaker Experiments",
-        "videoUrl": "",
-        "ownerId": "idealo",
-        "ownerUrl": "",
-        "ownerLogo": "idealo",
-        "projectId": "mle-sagemaker-experiments-tool",
-        "language": "python"
     }
 ]
