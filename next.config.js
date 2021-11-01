a529717290ef29de7a0e84edfd0503a6d305a03f
Conrad Magnus Kirschner <conrad.kirschner@idealo.de>
2021-11-01 15:57:28 +0100

    Added image optimization for static build - some minor fixes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const optimizedImages = require("next-optimized-images");
+const withPlugins = require("next-compose-plugins");
 
 /**
  * @type {import('next').NextConfig}
@@ -8,6 +10,25 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  images: {
+    disableStaticImages: true,
+  },
 };
-
-module.exports = nextConfig;
+const config = withPlugins(
+  [
+    [
+      optimizedImages,
+      {
+        // See: https://github.com/cyrilwanner/next-optimized-images#responsive
+        responsive: {
+          adapter: require("responsive-loader/sharp"),
+          sizes: [300, 320, 640, 960, 1200, 1800, 2400],
+          placeholder: true,
+          placeholderSize: 40,
+        },
+      },
+    ],
+  ],
+  nextConfig
+);
+module.exports = config;
