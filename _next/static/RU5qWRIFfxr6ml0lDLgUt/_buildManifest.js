f670f0d6b81fbd81860087e7cd82b54fdaea95fd
pjentsch <peter.jentsch@idealo.de>
2024-09-12 10:51:21 +0000

    Deploying to gh-pages from @ idealo/idealo.github.io@bb1ee8008a0af243b62d8c1e0e930c41669133b6 🚀

--- _next/static/RU5qWRIFfxr6ml0lDLgUt/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a6196ee5f884a883.css","static/chunks/pages/index-756943c3784b7299.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
